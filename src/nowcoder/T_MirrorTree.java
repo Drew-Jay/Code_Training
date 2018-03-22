ae8c0fac5b803fcb340fab371fbd3cd7d30a5d1e
12281138 <12281138@bjtu.edu.cn>
2018-03-22 16:48:39 +0800

    2018年3月22日

--- src/nowcoder/T_MirrorTree.java
+++ src/nowcoder/T_MirrorTree.java
@@ -3,8 +3,23 @@ package nowcoder;
 public class T_MirrorTree {
 
 	/**
-	 * @param args
+	 * 题目描述 操作给定的二叉树，将其变换为源二叉树的镜像。
 	 */
+
+	public void Mirror(TreeNode root) {
+		if(root != null){
+			reverse(root);
+			Mirror(root.left);
+			Mirror(root.right);
+		}
+	}
+	
+	public void reverse(TreeNode root){
+		TreeNode temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
