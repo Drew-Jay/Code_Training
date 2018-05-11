16721c939b6a9b2596cd61f761242b5b2bd31331
12281138 <12281138@bjtu.edu.cn>
2018-05-11 16:52:02 +0800

    2018年5月11日16:51:55

---
+++ src/com/dj/Arithmetic/DP_MaxSubSeq.java
@@ -0,0 +1,23 @@
+package com.dj.Arithmetic;
+
+public class DP_MaxSubSeq {
+
+    /**
+     * 最长公共子序列（longest common sequence）
+     * 和最长公共子串（longest common substring）
+     * 不是一回事儿
+     * 最长子序列不是最长子串
+     * 子序列可以不连续
+     * 子串必须连续
+     */
+
+    public  int[] lcs(int []s1,int []s2){
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        int[] s1 = {1, 3, 4, 5, 6, 7, 7, 8};
+        int[] s2 = {3, 5, 7, 4, 8, 6, 7, 8, 2};
+    }
+}
